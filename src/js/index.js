a16692f786d11cf71a543dfddd3f07abc49dd2d0
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-07 23:29:45 +0200

    imported imgs and created new img prototypes form them

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,16 @@
 import '../css/style.css';
+import img1 from '../img/watch1.png';
+import img2 from '../img/watch2.png';
+import img3 from '../img/watch1.png';
+
+const imageCarousel = (function () {
+  const watch1 = new Image();
+  const watch2 = new Image();
+  const watch3 = new Image();
+
+  watch1.src = img1;
+  watch2.src = img2;
+  watch3.src = img3;
+
+  const imgArray = [];
+})();
