6ae10b92b174cdaf48eed53648063dd4261dcfa9
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-07 21:22:32 +0200

    configured webpack, included html-loader
    now webpack will be able to compile images from html files

--- webpack.common.js
+++ webpack.common.js
@@ -24,6 +24,15 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
     ],
   },
 };
