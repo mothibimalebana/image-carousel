c13166d01d11875ee4114d50b3e0bd744643b56d
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-08 00:05:52 +0200

    created updateCarousel function
    the function updates the image along with it's color

--- src/js/index.js
+++ src/js/index.js
@@ -8,13 +8,29 @@ const imageCarousel = (function () {
   const watch2 = new Image();
   const watch3 = new Image();
   const display = document.getElementById('display');
+  const color = document.querySelector('.itemColor');
+  let currImage = 0;
+  function updateCarousel(indexOfImage) {
+    display.src = imgArray[indexOfImage].image.src;
+    color.textContent = `${imgArray[indexOfImage]}`;
+  }
 
   watch1.src = img1;
   watch2.src = img2;
   watch3.src = img3;
 
-  const imgArray = [img1, img2, img3];
-
-  display.src = imgArray[2];
-  console.log(imgArray);
+  const imgArray = [
+    {
+      color: 'Rose Gold',
+      image: watch1,
+    },
+    {
+      color: 'Grey',
+      image: watch2,
+    },
+    {
+      color: 'Dark Blue',
+      image: watch3,
+    },
+  ];
 })();
