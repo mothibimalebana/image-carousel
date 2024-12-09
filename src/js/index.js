262d84344859902c42bbcd12a7e123b2c6ec1041
mothibimalebana <mothibi.mm12@gmail.com>
2024-12-09 09:56:17 +0200

    whe the array reaches the end, it goes back to the first index

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,26 @@
 import '../css/style.css';
 import img1 from '../img/watch1.png';
 import img2 from '../img/watch2.png';
-import img3 from '../img/watch1.png';
+import img3 from '../img/watch3.png';
 
 const imageCarousel = (function () {
+  const rightButton = document.getElementById('right');
+  const leftButton = document.getElementById('left');
   const watch1 = new Image();
   const watch2 = new Image();
   const watch3 = new Image();
   const display = document.getElementById('display');
   const color = document.querySelector('.itemColor');
   let currImage = 0;
-  function updateCarousel(indexOfImage) {
-    display.src = imgArray[indexOfImage].image.src;
-    color.textContent = `${imgArray[indexOfImage]}`;
-  }
-
+  const getCurrImg = () => currImage;
+  const increaseIndex = () => currImage++;
+  const decreaseIndex = () => currImage--;
+  const updateIndex = (newIndex) => (currImage = newIndex);
   watch1.src = img1;
   watch2.src = img2;
   watch3.src = img3;
 
+  //array of images in our carousel
   const imgArray = [
     {
       color: 'Rose Gold',
@@ -33,4 +35,40 @@ const imageCarousel = (function () {
       image: watch3,
     },
   ];
+  //reads array index and displays the image and its color
+  function displayCarousel(indexOfArray) {
+    display.src = imgArray[indexOfArray].image.src;
+    color.textContent = `${imgArray[indexOfArray].color}`;
+  }
+  rightButton.addEventListener('click', () => {
+    increaseIndex();
+    displayCarousel(getCurrImg());
+  });
+  leftButton.addEventListener('click', () => {
+    decreaseIndex();
+    displayCarousel(getCurrImg());
+  });
+  return {
+    getCurrImg,
+    displayCarousel,
+    updateIndex,
+    increaseIndex,
+    decreaseIndex,
+  };
+})();
+
+const stateOfCarousel = (function () {
+  const dummyArray = ['img1', 'img2', 'img3'];
+  let i = 0;
+  while (i < dummyArray.length) {
+    if (i == dummyArray.length - 1) {
+      console.log(i);
+      console.log('wait 5 seconds');
+      i = 0;
+    } else {
+      console.log(i);
+      console.log('wait 5 seconds');
+      i++;
+    }
+  }
 })();
